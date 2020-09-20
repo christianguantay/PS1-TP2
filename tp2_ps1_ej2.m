bbfa523bc1943865c06c9e93c241fa9a6d18b515
christianguantay <65372063+christianguantay@users.noreply.github.com>
2020-09-19 22:37:38 -0300

    Add files via upload

---
+++ tp2_ps1_ej2.m
@@ -0,0 +1,44 @@
+clear all;
+close all;
+
+digits(128);
+[s,fs]=audioread('pista_01.wav');
+
+v = wgn(1,length(s),0.1);
+
+u = filter([0.8 , 0.2 , -0.1],1,v);
+
+
+for i = 1 : length(s)
+ x(i) = s(i) + v(i);
+end 
+
+M = [1,2,3,4,5,6];
+%%Calculo de las matrices de autocorrelacion dependiendo de M
+
+for i = 1 : length(M)    
+    aux_sum = 0;
+    for k = M(i) : length(u)-1    
+        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)')*flip (conj(u(k-M(i)+1:k)))) ;
+        aux_sum = aux_sum./(length(u)-M(i));
+        
+    end
+    auto_correlation{M(i)} = aux_sum;
+end
+
+%%Calculo del vector de correlacion cruzada
+
+for i = 1 : length(M)    
+    aux_sum = 0;
+    for k = M(i) : length(u)-1    
+        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)'))*conj(x(k)) ; %% NO SE SI DE d(n) EN LA FORMULA ES x(n)
+        aux_sum = aux_sum./(length(u)-M(i));
+    end
+    vector_correlation{M(i)} = aux_sum;
+end
+
+%%Calculo los coeficientes del filtro de Wiener (CREO QUE ES ASI)
+for i = 1 : length(M)    
+
+    wiener_coefficients{M(i)} = inv(auto_correlation{M(i)})*vector_correlation{M(i)};
+end
