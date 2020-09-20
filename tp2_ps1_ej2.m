940e20c4b10557aef251c9ef7a045a49ff42ab4d
christianguantay <65372063+christianguantay@users.noreply.github.com>
2020-09-19 23:14:31 -0300

    Add files via upload

--- tp2_ps1_ej2.m
+++ tp2_ps1_ej2.m
@@ -17,10 +17,10 @@ M = [1,2,3,4,5,6];
 %%Calculo de las matrices de autocorrelacion dependiendo de M
 
 for i = 1 : length(M)    
-    aux_sum = 0;
+    aux_sum = zeros(M(i));
     for k = M(i) : length(u)-1    
-        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)')*flip (conj(u(k-M(i)+1:k)))) ;
-        aux_sum = aux_sum./(length(u)-M(i));
+        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)')*flip (u(k-M(i)+1:k))) ;
+        %aux_sum = aux_sum/(length(u)-M(i));
         
     end
     auto_correlation{M(i)} = aux_sum;
@@ -29,16 +29,16 @@ end
 %%Calculo del vector de correlacion cruzada
 
 for i = 1 : length(M)    
-    aux_sum = 0;
+    aux_sum = zeros(M(i),1);
     for k = M(i) : length(u)-1    
-        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)'))*conj(x(k)) ; %% NO SE SI DE d(n) EN LA FORMULA ES x(n)
-        aux_sum = aux_sum./(length(u)-M(i));
+        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)'))*x(k) ; %% NO SE SI DE d(n) EN LA FORMULA ES x(n)
+        %aux_sum = aux_sum/(length(u)-M(i));
     end
     vector_correlation{M(i)} = aux_sum;
 end
 
 %%Calculo los coeficientes del filtro de Wiener (CREO QUE ES ASI)
 for i = 1 : length(M)    
-
+    
     wiener_coefficients{M(i)} = inv(auto_correlation{M(i)})*vector_correlation{M(i)};
 end
