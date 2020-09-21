fc667fb125722805f5fda63d6fcc24d9ad10b3b7
fede <federiconunezfrau@yahoo.com>
2020-09-21 10:08:53 -0300

    Version modularizada

--- tp2_ps1_ej2.m
+++ tp2_ps1_ej2.m
@@ -1,65 +1,56 @@
 clear all;
 close all;
+clc;
 
 blue = [0, 0.4470, 0.7410];
 orange = [0.8500, 0.3250, 0.0980];
 
-
+%% Se carga el archivo de audio
 [s,fs]=audioread('pista_01.wav');
+sigma = sqrt(0.1);
 
+%% Se crea el vector de ruido blanco gaussiano de media nula y varianza 0.1
+v = wgn(1,length(s), -10)';
 
-v = wgn(1,length(s), -10);
-
+%% Se filtra v para obtener u, señal del micrófono 2
 u = filter([0.8 , 0.2 , -0.1],1,v);
 
-
-for i = 1 : length(s)
- x(i) = s(i) + v(i);
-end 
+%% Señal del micrófono 1
+x = s + v;
 
 M = [1,2,3,4,5,6];
-%% Calculo de las matrices de autocorrelacion dependiendo de M
+%% Cálculo de las matrices de autocorrelacion dependiendo de M
 
-for i = 1 : length(M)    
-    aux_sum = zeros(M(i));
-    for k = M(i) : length(u)-1    
-        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)')*flip (u(k-M(i)+1:k))) ;
-        %
-    end
-    aux_sum = aux_sum/(length(u)-M(i));
-    auto_correlation{M(i)} = aux_sum;
+for i = 1 : length(M)
+    R = estimacion_matriz_autocorrelacion(M(i),u);
+    auto_correlation{M(i)} = R;
 end
 
-%% Calculo del vector de correlacion cruzada
+%% Cálculo del vector de correlacion cruzada
 
-for i = 1 : length(M)    
-    aux_sum = zeros(M(i),1);
-    for k = M(i) : length(u)-1    
-        aux_sum = aux_sum + (flip (u(k-M(i)+1:k)'))*x(k) ; %% NO SE SI DE d(n) EN LA FORMULA ES x(n)
-        
-    end
-    aux_sum = aux_sum/(length(u)-M(i));
-    vector_correlation{M(i)} = aux_sum;
+for i = 1 : length(M)
+    p = estimacion_matriz_correlacion_cruzada(M(i),u,x);
+    vector_correlation{M(i)} = p;
 end
 
-%% Calculo los coeficientes del filtro de Wiener (CREO QUE ES ASI)
+%% Cálculo los coeficientes del filtro de Wiener
 for i = 1 : length(M)    
     wiener_coefficients{M(i)} = inv(auto_correlation{M(i)})*vector_correlation{M(i)};
 end
 
-%% Calculo de y sombrero
+%% Cálculo de y sombrero
  for l = 1 : length(M)
     y_hat{M(l)} = filter(wiener_coefficients{M(l)},1,u); 
  end
  
-%% Calculo e(n)
+%% Cálculo e(n), señal de salida
 for k = 1:length(M)
     for n = 1 : length(x)
         e{M(k)}(n) = x(n) - y_hat{M(k)}(n);
     end 
 end
 
-%% Calculo Jmin
+%% Cálculo Jmin
 for k = 1:length(M) 
     Jmin(k) = e{M(k)}* (e{M(k)}');
     Jmin(k) = Jmin(k)/length(s);
@@ -76,7 +67,6 @@ title('Potencia de error')
 xlabel('Cantidad de muestras')
 ylabel('Magnitud')
 
-%% Audios
 audiowrite('pista_01_noiseTP2.wav',x,fs);
 
 audiowrite('pista_01_cleanedTP2M1.wav',e{1},fs);
